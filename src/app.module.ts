f64a8fca8ee097cabb07bb08e167fad9d7c3f026
Vitor Costa <vitor.costa@exaud.com>
2022-11-25 16:43:04 +0000

    added mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
@@ -10,6 +11,10 @@ import { AuthModule } from './auth/auth.module';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    // MongooseModule.forRoot(
+    //   'mongodb://localhost:27017',
+    //   // `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@127.0.0.1:27017/${process.env.MONGO_DB}`,
+    // ),
   ],
   controllers: [AppController],
   providers: [AppService],
