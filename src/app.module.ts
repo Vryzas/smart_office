f5609642c4499f2c975ee053476df2e9339aaacc
Vitor Costa <vitor.costa@exaud.com>
2022-12-16 15:52:53 +0000

    integrated click module in app

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ClickModule } from './click/click.module';
 
 @Module({
   imports: [
     AuthModule,
+    ClickModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
     MongooseModule.forRoot(
       // address should contain the mongo database container name for the network to identify it
-      'mongodb://localhost:27017',
-      // `mongodb://mongoadmin:pass@mongodb:27017?authsource=admin`,
+      // 'mongodb://root:example@mongodb:27017',
+      `mongodb://mongoadmin:pass@mongodb:27017?authsource=admin`,
+      // 'mongodb://localhost:27017/test?readPreference=primary&ssl=false&directConnection=true',
     ),
   ],
   controllers: [AppController],
